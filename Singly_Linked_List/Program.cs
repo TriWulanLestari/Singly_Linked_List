f2f1a36b96f470f601f20fd6c1a6dbd6efe6262a
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-07 17:04:13 +0700

    add a node in the list

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Singly_Linked_List
 {
@@ -12,6 +13,39 @@ namespace Singly_Linked_List
         public int rollNumber;
         public string name;
         public Node next;
+    }
+
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNote() // add a node in the list 
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            //if the node to be inserted is the first node
+            if(START == null) || rollNo == START.rollNumber)
+            {
+                if (START != null) && (rollNo == START.rollNumber))
+                        {
+                    Console.WriteLine();
+                    return;
+                }
+                namenode.next = START;
+                START = newnode;
+                return;
+            }
+        }
     }
     internal class Program
     {
